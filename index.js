2eddaf8a928cc2543c4ca59b065b23928a74a173
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-13 01:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ while (i < katzDeli.length){
   i++
 }
 if (katzDeli.length===0){
-  return ("the line is empty")
+  return ("There is nobody waiting to be served!")
 }else
   return (`Currently serving ${katzDeli.shift ()}.`)
 }
