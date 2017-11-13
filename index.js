6f3edb6cce2f3cce9b5a9e9f90144d2e3e65aa01
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-13 01:00:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = []
 
 function takeANumber (katzDeli, name){
   katzDeli.push (`${name}`)
-  return (`Welcome ${name} you are ${number} in line`)
+  return (`Welcome, ${name}. You are ${katzDeli.length} in line`)
 }
