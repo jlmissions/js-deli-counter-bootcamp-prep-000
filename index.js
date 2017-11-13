9e885e214caadb67e2a747fe3f5d42b2ce356243
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-13 01:21:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function currentLine (katzDeli){
   i++
 }
   if (katzDeli.length===0){
-  return ("The line is currently empty")
+  return ("The line is currently empty.")
 }else
 return (`The line is currently:` + line)
 }
