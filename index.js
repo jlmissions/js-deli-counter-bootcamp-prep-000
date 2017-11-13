9f60712c7584409559b5378c451468ae08727a1a
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-13 01:08:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function takeANumber (katzDeli, name){
   katzDeli.push (`${name}`)
   return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 }
+
+function nowServing (katzDeli){
+var i = 0
+while (i < katzDeli.length){
+  i++
+}
+if {
+  (katzDeli.length===0)
+  return ("the line is empty")
+}else 
+  return (`currently serving ${katzDeli.shift ()}`)
+}
